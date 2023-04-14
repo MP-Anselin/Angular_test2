a14c01acf14ac3b6c2663af076afc7560aa0a64d
MPdev <developperanselin.mp@gmail.com>
2023-04-14 08:45:18 +0200

    add posibility to add new user from dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import {FriendListService} from "../../services/friend-list.service";
 import {UserService} from "../../services/user.service";
 import {IUserCard} from "../../services/clients/interfaces/user.interface";
 import {IFriendCard} from "../../services/clients/interfaces/friend.interface";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -10,7 +12,13 @@ import {IFriendCard} from "../../services/clients/interfaces/friend.interface";
 })
 export class DashboardComponent implements AfterViewInit {
   subtitle: string;
-  constructor(private userService: UserService, private friendListService: FriendListService) {
+  signupForm: FormGroup;
+  constructor(
+    private userService: UserService,
+    private friendListService: FriendListService,
+    private fb: FormBuilder,
+    private authService: AuthenticationService
+    ) {
     this.userService
       .getUser()
       .subscribe((userList: IUserCard[]) => {
@@ -23,7 +31,27 @@ export class DashboardComponent implements AfterViewInit {
         this.friendListService.allFriend = friends;
       });
     this.subtitle = 'This is some text within a card block.';
+
+
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['JimmyTest1', Validators.required],
+      email: ['', Validators.required],
+      userRole: ['JimmyTest', Validators.required],
+      age: [24],
+      image: ['assets/images/users/user1.jpg'],
+      password: ['JimmyTest48', Validators.required],
+      isLog: [false],
+      status: [1]
+    });
   }
+
+  sendEmailToNewUser() {
+    console.log("this.signupForm.value => ", this.signupForm.value);
+    this.authService.register(this.signupForm.value);
+  }
+
 
   ngAfterViewInit() { }
 }
